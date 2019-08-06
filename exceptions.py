e228ce8db6c59b8b0133e2cb135b34a045dc16b1
JW-Ellis <gramnd464@gmail.com>
2019-08-05 22:36:46 -0500

    add working exception function

--- exceptions.py
+++ exceptions.py
@@ -21,16 +21,53 @@ def plineRelease():
         print("Answer with only y or n")
         print(plineRelease())
 
+def plineReportable():
+    reportable = input("Has the pipeline experienced two or more reportable releases, as defined in 195.50, within the previous five years? Answer with y/n: ")
+    if reportable == "y":
+        return True
+    elif reportable == "n": 
+        return False
+    else:
+        print("Answer with only y or n")
+        print(plineReportable())
 
+def plineWeld():
+    weld = input("Does the pipeline contain any electric resistance welded pipe manufactured prior to 1970 and if so, does it operate at a maximum operating pressure establisehd under 195.406 that corresponds to a stress level greater than 50 percent of the specified minimum yield strenght of the pipe. Answer with y/n: ")
+    if weld == "y":
+        return True
+    elif weld == "n": 
+        return False
+    else:
+        print("Answer with only y or n")
+        print(plineWeld())   
+
+def plineHca():
+    hca = input("Is the pipeline within 5 miles of public drinking water intakes or within 1 mile of environmentally senstitive areas? Answer with y/n: ")
+    if hca == "y":
+        return True
+    elif hca == "n": 
+        return False
+    else:
+        print("Answer with only y or n")
+        print(plineHca())
+        
 def exception(pipeDiameter, pipeLength):
-""" 
-The function will take pipeline length and diameter to determine
-if it meets exception 194.101(b)(1).
+    """
+This function will take pipeline diameter and pipeline length to determine Exception under 194.101(b).
 
-"""
+    """
     if (pipeDiameter <= 6.625) or (pipeLength < 10):
+        if plineRelease() == True:
+            return False
+        elif plineReportable() == True:
+            return False
+        elif plineWeld() == True:
+            return False
+        elif plineHca() == True:
+            return False
+        else:
+            return True
+    else:
+        return False
 
-        
-        
-        
-
+print(exception(4, 5))
